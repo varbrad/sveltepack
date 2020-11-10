559fb60c985e1ac19fcb23c90fdab177cda68278
Brad Davies <me@varbrad.dev>
2020-11-10 18:07:50 +0000

    Test initial svelte app

---
+++ snowpack.config.js
@@ -0,0 +1,14 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/#configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/dist',
+  },
+  plugins: ['@snowpack/plugin-svelte'],
+  // installOptions: {},
+  // devOptions: {},
+  // buildOptions: {},
+}
